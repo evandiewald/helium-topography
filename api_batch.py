95f35a93f1832afb47ee536bec9936bf41fe11a2
evandiewald <37876940+evandiewald@users.noreply.github.com>
2022-05-31 09:13:25 -0400

    same-maker ratio from last block, from main etl

--- api_batch.py
+++ api_batch.py
@@ -2,6 +2,8 @@ import sqlalchemy.exc
 
 from fastapi import FastAPI, APIRouter
 from fastapi.responses import JSONResponse
+
+import connection
 from models.tables import TopographyResults
 
 from starlette.requests import Request
@@ -23,7 +25,8 @@ app = FastAPI()
 engine = create_engine(os.getenv("POSTGRES_CONNECTION_STRING"))
 session = Session(engine)
 
-
+etl_engine = connection.connect()
+etl_session = Session(etl_engine)
 
 
 
@@ -47,29 +50,37 @@ async def topography(request: Request, response: Response, address: str):
 
 @router.get("/witnesses/{address}")
 async def witnesses(request: Request, response: Response, address: str):
-    sql_same_maker = f"""
-    select
+    sql_same_maker = f"""with gateway_details as (select last_block, payer from gateway_inventory where address = '{address}'),
 
-    rx_address as address,
-
-    sum(
-    	CASE WHEN
-    	tx_payer = rx_payer THEN 1
-    	ELSE 0
-    	END
-    )::float / count(tx_address) as same_maker_ratio, 
-
-    count(distinct(tx_address)) as n_witnessed
-
-    from detailed_receipts
-
-    where rx_address = '{address}'
-     group by rx_address;"""
+    hashes as
+    
+    (select transaction_hash from transaction_actors 
+    where actor_role = 'witness'::transaction_actor_role 
+    and block > (select last_block from gateway_details) - 5000 
+    and actor = '{address}'),
+    
+    metadata as
+    
+    (select fields->'path'->0->'witnesses' as w from transactions where hash in (select * from hashes)),
+    
+    results as
+    (select
+    (select array_agg(t -> 'gateway') from jsonb_array_elements(w) as x(t)) as gateway from metadata),
+    
+    witnesses_unnest as
+    (select distinct(text(unnest(gateway))) as witness from results where gateway is not NULL),
+    
+    witnesses as
+    (select substring(witness from 2 for LENGTH(witness)-2) as witness from witnesses_unnest),
+    
+    makers as
+    (select payer from witnesses b join gateway_inventory g on b.witness = g.address)
+    
+    select sum(case when payer = (select payer from gateway_details) then 1 else 0 end)::float / count(*) as same_maker_ratio, count(*) as n_witnessed from makers;"""
 
     try:
-        res = session.execute(sql_same_maker).one()
-        print(res)
-        return JSONResponse({"result": {"address": res[0], "different_maker_ratio": round(1 - res[1], 2), "n_witnessed": int(res[2])}})
+        res = etl_session.execute(sql_same_maker).one()
+        return JSONResponse({"result": {"address": address, "different_maker_ratio": round(1 - res[0], 2), "n_witnessed": int(res[1])}})
     except sqlalchemy.exc.NoResultFound:
         return JSONResponse({"NoResultFound": "No witness result found for this address"}, status_code=500)
 
