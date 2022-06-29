6c0b1b92c76c2e47fb8b86b55d834946ea2612aa
evandiewald <37876940+evandiewald@users.noreply.github.com>
2022-06-29 09:06:10 -0400

    add maintenance mode to api while ETL is offline

--- api_batch.py
+++ api_batch.py
@@ -21,15 +21,22 @@ import pandas as pd
 
 load_dotenv()
 
+MAINTENANCE_MODE = True
+
+
 router = APIRouter(prefix="/api/v1")
 app = FastAPI()
 
 
-lite_engine = create_engine(os.getenv("POSTGRES_CONNECTION_STRING"))
-lite_session = sessionmaker(lite_engine)
 
-etl_engine = connection.connect()
-etl_session = sessionmaker(etl_engine)
+if not MAINTENANCE_MODE:
+    lite_engine = create_engine(os.getenv("POSTGRES_CONNECTION_STRING"))
+    lite_session = sessionmaker(lite_engine)
+
+    etl_engine = connection.connect()
+    etl_session = sessionmaker(etl_engine)
+else:
+    print("\n\nStarting server in MAINTENANCE MODE. Requests will be processed, but results will be empty.\n\n")
 
 
 def get_results_for_hotspot(session: Session, address: str):
@@ -43,46 +50,52 @@ def get_results_for_hotspot(session: Session, address: str):
 
 @router.get("/topography/{address}")
 async def topography(request: Request, response: Response, address: str):
-    with lite_session() as sess:
-        result = get_results_for_hotspot(sess, address)
-        if result:
-            return result
-        else:
-            return JSONResponse({"NoResultFound": "No topography result found for this address"}, status_code=500)
+    if MAINTENANCE_MODE:
+        return JSONResponse({"NoResultFound": "Systems under maintenance."}, status_code=500)
+    else:
+        with lite_session() as sess:
+            result = get_results_for_hotspot(sess, address)
+            if result:
+                return result
+            else:
+                return JSONResponse({"NoResultFound": "No topography result found for this address"}, status_code=500)
 
 
 @router.get("/witnesses/{address}")
 async def witnesses(request: Request, response: Response, address: str):
-    sql_same_maker = f"""with gateway_details as (select last_block, payer from gateway_inventory where address = '{address}'),
-    
-    hashes as
-    
-    (select transaction_hash from transaction_actors 
-    where actor_role = 'witness'::transaction_actor_role 
-    and block > (select last_block from gateway_details) - 7500 
-    and actor = '{address}'),
-    
-    metadata as
-    (select text(fields->'path'->0->'challengee') as w from transactions where hash in (select * from hashes)),
-    
-    
-    witnesses as
-    (select substring(w from 2 for LENGTH(w)-2) as witness from metadata),
-    
-    
-    
-    makers as
-    (select payer from witnesses b join gateway_inventory g on b.witness = g.address)
-    
-    
-    select sum(case when payer = (select payer from gateway_details) then 1 else 0 end)::float / count(*) as same_maker_ratio, count(*) as n_witnessed from makers;"""
+    if MAINTENANCE_MODE:
+        return JSONResponse({"NoResultFound": "Systems under maintenance."}, status_code=500)
+    else:
+        sql_same_maker = f"""with gateway_details as (select last_block, payer from gateway_inventory where address = '{address}'),
+        
+        hashes as
+        
+        (select transaction_hash from transaction_actors 
+        where actor_role = 'witness'::transaction_actor_role 
+        and block > (select last_block from gateway_details) - 7500 
+        and actor = '{address}'),
+        
+        metadata as
+        (select text(fields->'path'->0->'challengee') as w from transactions where hash in (select * from hashes)),
+        
+        
+        witnesses as
+        (select substring(w from 2 for LENGTH(w)-2) as witness from metadata),
+        
+        
+        
+        makers as
+        (select payer from witnesses b join gateway_inventory g on b.witness = g.address)
+        
+        
+        select sum(case when payer = (select payer from gateway_details) then 1 else 0 end)::float / count(*) as same_maker_ratio, count(*) as n_witnessed from makers;"""
 
-    with etl_session() as sess:
-        try:
-            res = sess.execute(sql_same_maker).one()
-            return JSONResponse({"result": {"address": address, "different_maker_ratio": round(1 - res[0], 2), "n_witnessed": int(res[1])}})
-        except sqlalchemy.exc.NoResultFound:
-            return JSONResponse({"NoResultFound": "No witness result found for this address"}, status_code=500)
+        with etl_session() as sess:
+            try:
+                res = sess.execute(sql_same_maker).one()
+                return JSONResponse({"result": {"address": address, "different_maker_ratio": round(1 - res[0], 2), "n_witnessed": int(res[1])}})
+            except sqlalchemy.exc.NoResultFound:
+                return JSONResponse({"NoResultFound": "No witness result found for this address"}, status_code=500)
 
 
 
